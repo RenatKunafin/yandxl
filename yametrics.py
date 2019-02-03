d71db387623e0937a5b891610838475e9789511f
Кунафин Ренат Ильгизович <Kunafin.R.Il@sberbank.ru>
2019-02-03 21:16:20 +0300

    Add correct dimensions and metrics

--- yametrics.py
+++ yametrics.py
@@ -9,23 +9,43 @@ logging.basicConfig(level=logging.DEBUG)
 class Yametrics:
     def __init__(self):
         self.url = os.environ.get('API_ROOT_URL')
+        self.url_logs = os.environ.get('API_ROOT_URL2')
         self.token = os.environ.get('YANDEX_TOKEN')
         self.headers = {'Authorization': 'OAuth  ' + self.token}
-        self.metrics = ['ym:s:visits', 'ym:s:pageviews', 'ym:s:users']
+        self.counter_id = os.environ.get('COUNTER')
+        self.dimensions = [
+            'ym:s:paramsLevel1',
+            'ym:s:paramsLevel2',
+            'ym:s:paramsLevel3',
+            'ym:s:paramsLevel4',
+            'ym:s:paramsLevel5',
+            'ym:s:paramsLevel6'
+        ]
+        self.metrics = [
+            'ym:s:visits',
+            'ym:s:users',
+            'ym:s:sumParams',
+            'ym:s:paramsNumber',
+            'ym:s:avgParams',
+            'ym:s:bounceRate',
+            'ym:s:pageDepth',
+            'ym:s:avgVisitDurationSeconds'
+        ]
 
     def request_metrics(self):
         params = {
             "direct_client_logins": os.environ.get('LOGIN'),
             "ids": os.environ.get('COUNTER'),
+            "dimensions": ",".join(self.dimensions),
             "metrics": ",".join(self.metrics)
         }
         try:
             response = requests.get(url=self.url, headers=self.headers, params=params)
             json = response.json()
-            print(response)
             if response.status_code == 200:
                 pprint.pprint(json)
+                return json
             else:
-                print('Error')
+                pprint.pprint(json)
         except Exception as e:
             print(e)
--- yandxl.py
+++ yandxl.py
@@ -1,5 +1,14 @@
 from yametrics import Yametrics
 
+# metrics = [
+#     # 'ym:s:visits',
+#     # 'ym:s:users',
+#     # 'ym:s:pageviews',
+#     'ym:s:goal<goal_id>reaches',
+#     'ym:s:goal<goal_id>visits',
+#     'ym:s:goal<goal_id>users'
+# ]
 
-metrics = Yametrics()
-metrics.request_metrics()
+ym = Yametrics()
+ym.request_metrics()
+# ym.get_logs()
