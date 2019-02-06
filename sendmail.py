3574251a79450f0de529d9b299a3a0ccdb2e7895
Кунафин Ренат Ильгизович <Kunafin.R.Il@sberbank.ru>
2019-02-06 23:30:11 +0300

    add send excel-report by yandex mail

---
+++ sendmail.py
@@ -0,0 +1,33 @@
+import smtplib
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.utils import COMMASPACE, formatdate
+
+
+def send_mail(send_from, send_to, subject, text, password, files=None):
+
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = COMMASPACE.join(send_to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(text, _charset="UTF-8"))
+
+    for f in files or []:
+        with open(f, "rb") as file:
+            part = MIMEApplication(
+                file.read(),
+                Name=basename(f)
+            )
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+        msg.attach(part)
+
+    smtp = smtplib.SMTP_SSL('smtp.yandex.com', 465)
+    smtp.set_debuglevel(1)
+    smtp.ehlo()
+    smtp.login(send_from, password)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.close()
--- yandxl.py
+++ yandxl.py
@@ -3,6 +3,7 @@ from excel import Excel
 import os
 import sys
 from configparser import ConfigParser
+from sendmail import send_mail
 
 base_path = os.path.dirname(os.path.abspath(__file__))
 config_path = os.path.join(base_path, "params.ini")
@@ -19,4 +20,15 @@ data = ym.request_metrics()
 
 e = Excel(cfg, data)
 e.init_wb()
-# e.write_to_wb()
+e.write_to_wb()
+
+server = cfg.get('smtp', 'SERVER')
+port = cfg.get('smtp', 'PORT')
+from_address = cfg.get('smtp', 'FROM')
+to_address = cfg.get('smtp', 'TO').split(',')
+subject = cfg.get('smtp', 'SUBJECT')
+text = cfg.get('smtp', 'TEXT')
+file = cfg.get('smtp', 'PATH') + cfg.get('excel', 'WB_NAME')
+password = cfg.get('smtp', 'PASS')
+
+send_mail(from_address, to_address, subject, text, password, [file])
